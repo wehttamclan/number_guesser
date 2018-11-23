6dd54ce7417531cf3b6a59019430902df1443854
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-11-23 11:54:41 -0700

    Disables buttons that arent in use.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,10 +2,10 @@ var min = 1;
 var max = 100;
 var randomNumber = randomNumberGenerator(min, max);
 var guessInput = document.getElementById("guess-input");
-var submitGuess = document.getElementById("submit-guess");
 var displayGuess = document.getElementById("display-guess");
 var displayHint = document.getElementById("hint");
 var feedback = document.getElementById("feedback");
+var guessButton = document.getElementById("submit-guess");
 var resetButton = document.getElementById("reset-button");
 var clearButton = document.getElementById("clear-guess");
 
@@ -17,7 +17,7 @@ function randomNumberGenerator(min, max) {
 
 function checkGuess() {
   let guess = guessInput.value;
-  if(isNaN(guess) || guess == '') {
+  if(isNaN(parseInt(guess)) || guess == '') {
     displayGuess.innerText = `-`;
     displayHint.innerText = 'Please enter a valid number';
     feedback.style.display = "block";
@@ -48,6 +48,19 @@ function resetGame() {
 
 function clearGuess() {
   guessInput.value = '';
+  enableButtons();
 }
-submitGuess.addEventListener('click', checkGuess);
+
+function enableButtons() {
+  if(isNaN(parseInt(guessInput.value))) {
+    guessButton.disabled = true;
+    clearButton.disabled = true;
+  } else {
+    guessButton.disabled = false;
+    clearButton.disabled = false;
+  };
+};
+
+guessButton.addEventListener('click', checkGuess);
 resetButton.addEventListener('click', resetGame)
+guessInput.addEventListener('input', enableButtons)
