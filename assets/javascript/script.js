f3a3f7b355e565953ef6af3e0bd1047fb6737645
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-11-22 20:16:15 -0700

    Shows feedback on correct guess.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,25 @@
 var min = 1;
 var max = 100;
+var randomNumber = randomNumberGenerator(min, max);
 var guessInput = document.getElementById("guess-input");
 var submitGuess = document.getElementById("submit-guess");
+var displayGuess = document.getElementById("display-guess")
+var feedback = document.getElementById("feedback")
+
+document.onload = randomNumberGenerator();
 
 function randomNumberGenerator(min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 };
 
 function checkGuess() {
-  console.log("hello")
+  let guess = guessInput.value;
+  let guessText = document.createTextNode(guessInput.value);
+  if(guess == randomNumber) {
+    displayGuess.appendChild(guessText);
+    feedback.style.display = "block";
+  }
+  else console.log(`"Try again ${randomNumber}"`)
 };
 
 submitGuess.addEventListener('click', checkGuess);
