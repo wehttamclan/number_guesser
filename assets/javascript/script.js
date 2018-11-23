c8f3c61d5a1053d728fe4096a753d80dd9c00817
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-11-23 07:47:56 -0700

    Trying to change feedback text with appendChild, but might have to change to innerHTML.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,6 +4,7 @@ var randomNumber = randomNumberGenerator(min, max);
 var guessInput = document.getElementById("guess-input");
 var submitGuess = document.getElementById("submit-guess");
 var displayGuess = document.getElementById("display-guess")
+var displayHint = document.getElementById("hint")
 var feedback = document.getElementById("feedback")
 
 document.onload = randomNumberGenerator();
@@ -15,11 +16,22 @@ function randomNumberGenerator(min, max) {
 function checkGuess() {
   let guess = guessInput.value;
   let guessText = document.createTextNode(guessInput.value);
-  if(guess == randomNumber) {
-    displayGuess.appendChild(guessText);
+  if(isNaN(guess) || guess == '') {
+    let errorText = document.createTextNode("Please enter a valid number");
+    displayHint.innerHTML = '';
+    displayHint.appendChild(errorText);
     feedback.style.display = "block";
-  }
-  else console.log(`"Try again ${randomNumber}"`)
+  } else if(guess > randomNumber) {
+    displayGuess.appendChild(guessText);
+    let hintText = document.createTextNode("That is too high");
+    displayHint.appendChild(hintText);
+    feedback.style.display = "block";
+  } else if(guess < randomNumber) {
+    displayGuess.appendChild(guessText);
+    let hintText = document.createTextNode("That is too low");
+    displayHint.appendChild(hintText);
+    feedback.style.display = "block";
+  } else console.log(`"Try again ${randomNumber}"`)
 };
 
 submitGuess.addEventListener('click', checkGuess);
